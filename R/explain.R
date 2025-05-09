51808e530b677f4ca2b940a9cedeb8f50e86a2ad
Thomas Brazier <brazier.thomas@gmail.com>
2025-05-09 17:39:12 +0200

    Add an explain R6 class for innsigth module

---
+++ R/explain.R
@@ -0,0 +1,160 @@
+#' ABCNN `explain` object
+#' A R6 class object
+#'
+#' @param abcnn An `abcnn` object
+#' @param method A feature attribution method, as named in the `ìnnsight` R package
+#' including cw (default), grad, smoothgrad, intgrad, expgrad, lrp, deeplift,
+#' deepshap, shap, lime
+#'
+#'
+#' @slot converter Stores the `innsigth::converter` object
+#'
+#' @import torch
+#' @import luz
+#' @import ggplot2
+#' @import innsight
+#' @import plotly
+#' @import R6Class
+#' @import RColorBrewer
+#'
+#' @return an `explain` object
+#' @export
+#'
+explain = R6::R6Class("explain",
+                    public = list(
+                      x = NULL,
+                      method = NULL,
+                      converter = NULL,
+
+                      initialize = function(x,
+                                            method = "cw") {
+                        self$method = method
+
+                        # Convert the abccn object passed as input
+
+                        if (x$method == "monte carlo dropout") {
+
+                        }
+
+                        if (x$method == "concrete dropout") {
+                          # FOR A CONCRETE MODEL
+                          model = x$fitted$model
+                          # model$modules
+                          model_concrete = model$modules[[1]]
+                          # model_concrete
+                          model_concrete = model_concrete$concrete_dropout
+                          # model_concrete
+
+                          model_sequential = nn_sequential(model_concrete[[1]]$linear)
+
+                          for (i in 2:x$num_hidden_layers) {
+                            mod = model_concrete[[i]]$linear
+                            model_sequential$add_module(name = i - 1, module = mod)
+                          }
+
+                          # model_sequential = nn_sequential(model_concrete$conc_drop1$linear,
+                          #                                  model_concrete$conc_drop2$linear,
+                          #                                  model_concrete$conc_drop3$linear)
+                        }
+
+                        if (x$method == "deep ensemble") {
+                          # FOR A DEEP ENSEMBLE MODEL
+                          model = x$fitted$model
+                          # Extract one model
+                          model_one = model$model_list[[1]]
+                          # Extract nn_sequential
+                          model_sequential = model_one$mlp
+                        }
+
+                        model_input_dim = dim(x$sumstats.train)[2]
+                        converter = innsigth::convert(model_sequential, input_dim = model_input_dim)
+
+                        self$converter = converter
+
+                        self$print()
+
+                        # END ON INIT
+                      },
+
+                      # Print the converter
+                      print = function() {
+                        self$converter$print()
+                      },
+
+                      #' Train the neural network
+                      #'
+                      #' The method is run on a `data` object (see `innsight` manual)
+                      #' @param data (array, data.frame, torch_tensor or list)
+                      #' The data to which the method is to be applied. These must have the same format as the input data of the passed model to the converter object. This means either
+                      #' an array, data.frame, torch_tensor or array-like format of size (batch_size, dim_in), if e.g., the model has only one input layer, or
+                      #' a list with the corresponding input data (according to the upper point) for each of the input layers.
+                      #'
+                      #' Note: For the model-agnostic methods, only models with a single input and output layer is allowed!
+                      #'
+                      #' @param data_ref (array, data.frame or torch_tensor)
+                      #' The dataset to which the method is to be applied. These must have the same format as the input data of the passed model and has to be either matrix, an array, a data.frame or a torch_tensor.
+                      #' Note: For the model-agnostic methods, only models with a single input and output layer is allowed!
+                      run = function(data, data_ref) {
+                        # Methods: cw (default), grad, smoothgrad, intgrad, expgrad, lrp, deeplift,
+                        # deepshap, shap, lime
+                        if (self$method == "cw") {
+                          result = innsight::run_cw(self$converter) # no data argument is needed
+                        }
+                        if (self$method == "grad") {
+                          result = innsight::run_grad(self$converter, data)
+                        }
+                        if (self$method == "smoothgrad") {
+                          result = innsight::run_smoothgrad(self$converter, data)
+                        }
+                        if (self$method == "intgrad") {
+                          result = innsight::run_intgrad(self$converter, data)
+                        }
+                        if (self$method == "expgrad") {
+                          result = innsight::run_expgrad(self$converter, data)
+                        }
+                        if (self$method == "lrp") {
+                          result = innsight::run_lrp(self$converter, data)
+                        }
+                        if (self$method == "deeplift") {
+                          result = innsight::run_deeplift(self$converter, data)
+                        }
+                        if (self$method == "deepshap") {
+                          result = innsight::run_deepshap(self$converter, data)
+                        }
+                        if (self$method == "shap") {
+                          result = innsight::run_shap(self$converter, data, data_ref)
+                        }
+                        if (self$method == "lime") {
+                          result = innsight::run_lime(self$converter, data, data_ref)
+                        }
+
+                        self$result = result
+
+                        return(result)
+                      },
+
+                      #' Get the results of the Feature Attribution method
+                      #'
+                      get_result = function() {
+                        innsigth::get_result(result)
+                      },
+
+                      #' Plot the results of the Feature Attribution method
+                      #'
+                      #' @param as_plotly If `TRUE`, plot the figure as a plotly object (default = `FALSE`)
+                      #'
+                      plot = function(as_plotly = FALSE) {
+                        # Get the results as an array
+                        res = self$get_result(result)
+                        # Plot individual results
+                        innsigth::plot(result)
+
+                        # Plot a aggregated plot of all given data points in argument 'data'
+                        innsigth::plot_global(result)
+                        innsigth::boxplot(result) # alias of `plot_global` for tabular and signal data
+                        # Interactive plots can also be created for both methods
+                        innsigth::plot(result, as_plotly = TRUE)
+                      }
+                    )
+)
+
