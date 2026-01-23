5a6d5e407a2bf3e1a325c56b1efcf45a6cf3f3a9
Thomas Brazier <brazier.thomas@gmail.com>
2026-01-23 22:29:40 +0100

    Update tests

--- tests/testthat/test-abc-methods.R
+++ tests/testthat/test-abc-methods.R
@@ -14,9 +14,9 @@ for (m in methods) {
       stat2 = theta_training$param1^2 + rnorm(n_samples, 0, 0.1)
     )
     sumstats_observed = data.frame(stat1 = 0.4, stat2 = 0.2)
-    
-    num_posterior_samples= 50 
-    
+
+    num_posterior_samples= 50
+
     abc = abcnn$new(
       theta_training,
       sumstats_training,
@@ -30,17 +30,17 @@ for (m in methods) {
       num_posterior_samples = num_posterior_samples,
       verbose = FALSE
     )
-    
+
     # Test fitting
     expect_no_error(abc$fit())
     expect_true(abc$n_train == 7000)
     expect_true(abc$n_obs == 1)
-    
+
     # Test prediction
     expect_no_error(abc$predict())
     expect_equal(dim(abc$predictive_mean), c(1, 1))
     expect_equal(dim(abc$epistemic_uncertainty), c(1, 1))
-    
+
     # Test posterior sampling
     # expect_no_error(abc$posterior())
     if (m %in% c("monte carlo dropout")) {
@@ -50,12 +50,12 @@ for (m in methods) {
         expect_equal(dim(abc$posterior_samples), c(num_posterior_samples, 1, 2))
       }
     }
-    
+
     # Test credible intervals
     expect_no_error(abc$predictions())
     expect_equal(dim(abc$predictions()), c(1, 11))
   })
-  
+
 }
 
 test_that("Method-specific parameters are validated", {
@@ -67,7 +67,7 @@ test_that("Method-specific parameters are validated", {
     stat2 = theta_training$param1^2 + rnorm(n_samples, 0, 0.1)
   )
   sumstats_observed = data.frame(stat1 = 0.4, stat2 = 0.2)
-  
+
   # Test Monte Carlo Dropout parameter validation
   expect_error(
     abcnn$new(
@@ -76,7 +76,7 @@ test_that("Method-specific parameters are validated", {
     ),
     "The 'dropout' rate must be between 0.1 and 0.5."
   )
-  
+
   expect_error(
     abcnn$new(
       theta_training, sumstats_training, sumstats_observed,
@@ -84,7 +84,7 @@ test_that("Method-specific parameters are validated", {
     ),
     "The 'dropout' rate must be between 0.1 and 0.5."
   )
-  
+
 })
 
 test_that("Method-specific outputs are correct", {
@@ -96,7 +96,7 @@ test_that("Method-specific outputs are correct", {
     stat2 = theta_training$param1^2 + rnorm(n_samples, 0, 0.1)
   )
   sumstats_observed = data.frame(stat1 = 0.4, stat2 = 0.2)
-  
+
   # Test Monte Carlo Dropout outputs
   abc_mc = abcnn$new(
     theta_training, sumstats_training, sumstats_observed,
@@ -104,13 +104,14 @@ test_that("Method-specific outputs are correct", {
   )
   abc_mc$fit()
   abc_mc$predict()
-  
+
   # MC dropout doesn't have aleatoric uncertainty
   expect_true(is.na(abc_mc$aleatoric_uncertainty))
   expect_true(!is.na(abc_mc$epistemic_uncertainty))
-  expect_true(is.na(abc_mc$overall_uncertainty))
-  
-  
+  expect_true(!is.na(abc_mc$overall_uncertainty))
+  expect_equal(abc_mc$epistemic_uncertainty, abc_mc$overall_uncertainty)
+
+
   # Test Concrete Dropout outputs
   abc_cd = abcnn$new(
     theta_training, sumstats_training, sumstats_observed,
@@ -118,7 +119,7 @@ test_that("Method-specific outputs are correct", {
   )
   abc_cd$fit()
   abc_cd$predict()
-  
+
   expect_true(!is.na(abc_cd$aleatoric_uncertainty))
   expect_true(!is.na(abc_cd$epistemic_uncertainty))
   expect_true(!is.na(abc_cd$overall_uncertainty))
@@ -130,11 +131,11 @@ test_that("Method-specific outputs are correct", {
   )
   abc_de$fit()
   abc_de$predict()
-  
+
   expect_true(!is.na(abc_de$aleatoric_uncertainty))
   expect_true(!is.na(abc_de$epistemic_uncertainty))
   expect_true(!is.na(abc_de$overall_uncertainty))
-  
+
   # Test TabNet-ABC outputs
   abc_tab = abcnn$new(
     theta_training, sumstats_training, sumstats_observed,
@@ -142,10 +143,11 @@ test_that("Method-specific outputs are correct", {
   )
   abc_tab$fit()
   abc_tab$predict()
-  
+
   expect_true(is.na(abc_tab$aleatoric_uncertainty))
   expect_true(!is.na(abc_tab$epistemic_uncertainty))
-  expect_true(is.na(abc_tab$overall_uncertainty))
+  expect_true(!is.na(abc_tab$overall_uncertainty))
+  expect_equal(abc_tab$epistemic_uncertainty, abc_tab$overall_uncertainty)
 })
 
 
@@ -158,6 +160,6 @@ test_that("Methods handle different data dimensions correctly", {
     stat2 = theta_training$param1^2 + rnorm(n_samples, 0, 0.1)
   )
   sumstats_observed = data.frame(stat1 = 0.4, stat2 = 0.2)
-  
+
 }
 )
